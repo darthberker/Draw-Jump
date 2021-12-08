6c5cabb86a3c29aa01168816e6a3009ee9626d34
Berker Topaloğlu <bebekerbfh@gmail.com>
2021-12-08 13:53:10 +0300

    DrawJump

---
+++ DrawJump/Assets/Files/Scripts/Char.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char : MonoBehaviour
+{
+    public GameObject karakter;
+    int sayici = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.gravity = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Physics2D.gravity = new Vector2(0,-40);
+            launchChar();
+            sayici++;
+        }
+        
+    }
+    private void launchChar()
+    {
+        if (Input.GetMouseButtonUp(0)&& sayici<1)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(70f,100f),Random.Range(50f,90f));
+            
+        }
+    }
+
+    
+        
+    
+}
---
+++ DrawJump/Assets/Files/Scripts/Sonraki.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sonraki : MonoBehaviour
+{
+    
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        int nextBuildIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextBuildIndex);
+    }
+}
